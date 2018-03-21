b663e8eab58d328e43149f3148db5b0c88313c88
mike <themikegracia@gmail.com>
2018-03-20 22:27:59 -0400

    fixes and add preview functionality

--- ask-whet.py
+++ ask-whet.py
@@ -3,7 +3,7 @@ from flask_ask import Ask, statement, question, session
 import json
 import requests
 import time
-import unidecode
+#import unidecode
 import websocket
 
 app = Flask(__name__)
@@ -14,15 +14,18 @@ def get_info():
     try:
         conn = websocket.create_connection('ws://localhost:7999/chat/websocket?id=ask-whet', timeout=6)
         answer = ""
-        response = conn.recv()
-        print(str(response))
-        j = json.loads(response)
-        if 'status' in j:
-            for c in j['status']:
-                answer += 'Channel {}... {} percent. '.format( c['c_id'], c['percent'])
-            conn.close(reason='done listening')
+        while len(answer) == 0:
+            response = conn.recv()
+            print(str(response))
+            j = json.loads(response)
+            if 'status' in j:
+                for c in j['status']:
+                    answer += 'Channel {}... {} percent. '.format( c['alias'], c['percent'])
+                conn.close(reason='done listening')
+
         return answer
     except Exception as e:
+        print(e)
         return e
 
 def set_weather(setas="storm"):
@@ -35,20 +38,32 @@ def set_weather(setas="storm"):
     print(str(answer))
     conn.send('{"update":' + json.dumps(answer) + '}')
     conn.close(reason='done listening')
-    return str(answer)
+
+def set_preview(setas=500):
+    conn = websocket.create_connection('ws://localhost:7999/chat/websocket?id=ask-whet', timeout=6)
+    conn.send('{"request": "light_schedule" }')
+    answer = str(conn.recv())
+    print(answer)
+    answer = json.loads(answer)
+    for c in answer['channels']:
+        c['preview']['value'] = setas
+        c['preview']['active'] = True
+    print(str(answer))
+    conn.send('{"update":' + json.dumps(answer) + '}')
+    conn.close(reason='done listening')
 
 
 @app.route('/')
 def homepage():
-    return get_info() + set_weather()
+    return "ayyyyyyy"
 
     
 
 
 @ask.launch
 def start_skill():
-    #welcome_message = "Hi, Welcome to Whet!. I am your slave."  #this should be a question
-    welcome_message = get_info()
+    welcome_message = "Connected. "  #this should be a question
+    #welcome_message = get_info()
     return question(welcome_message)
 
 @ask.intent("AMAZON.YesIntent")
@@ -64,6 +79,23 @@ def no_intent():
 def help_intent():
     return statement('Welcome to Costco... I love you.')
 
+@ask.intent("StormIntent")
+def storm_intent():
+    set_weather(setas="storm")
+    return statement("Storms a comming!")
+
+@ask.intent("NoWeatherIntent")
+def no_weather_intent():
+    set_weather(setas="normal")
+    return statement("Ending weather")
+
+@ask.intent('LightAdjustmentIntent', convert={'percent': int})
+def weather(percent):
+    print(percent)
+    pwm = (percent / 100) * 4095
+    set_preview(pwm)
+    return statement('Setting lights to {}'.format(pwm))
+
 
 
 
@@ -74,4 +106,4 @@ def help_intent():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
