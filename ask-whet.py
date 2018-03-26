13a7b4843039d2ecbce52af6dcbe532e2b5f443b
mike <themikegracia@gmail.com>
2018-03-26 11:54:10 -0400

    methods for reused code

--- ask-whet.py
+++ ask-whet.py
@@ -5,77 +5,96 @@ import requests
 import time
 #import unidecode
 import websocket
+import logging
 
 app = Flask(__name__)
 ask = Ask(app, '/ask')
 url = 'ws://localhost:7999/chat/websocket?id=ask-whet'
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 
-def get_info():
+def create_connection():
     try:
         conn = websocket.create_connection(url, timeout=6)
-        answer = ""
-        while len(answer) == 0:
-            response = conn.recv()
-            print(str(response))
-            j = json.loads(response)
-            if 'status' in j:
-                for c in j['status']:
-                    answer += 'Channel {}... {} percent. '.format( c['alias'], c['percent'])
-                conn.close(reason='done listening')
-
-        return answer
+        return conn
     except Exception as e:
         print(e)
         return e
 
+
+def get_response(conn, keyword):
+    i = 0
+    while i < 5:
+        response = conn.recv()
+        j = json.loads(response)
+        if keyword in j:
+            return j
+        else:
+            print(j)
+            i += 1
+    print('error getting response')
+
+
+def close_connection(conn):
+    conn.close(reason='done listening')
+    print("Connection closed")
+
+
+def get_info():
+    conn = create_connection()
+    response = get_response(conn, 'status')
+    speak = "Status..."
+    for c in response['status']:
+        speak += 'Channel {}... {} percent. '.format(
+            c['alias'], c['percent'])
+
+    close_connection(conn)
+
+    return speak
+
+
 def set_runmode(setas="normal"):
-    conn = websocket.create_connection(url, timeout=6)
+    conn = create_connection()
     conn.send('{"request": "settings" }')
-    answer = str(conn.recv())
-    print(answer)
-    answer = json.loads(answer)
-    answer['settings']['runmode'] = setas
-    print(str(answer))
-    conn.close(reason='done listening')
-    conn = websocket.create_connection(url, timeout=6)
-    conn.send('{"update":' + json.dumps(answer) + '}')
-    conn.close(reason='done listening')
+    response = get_response(conn, 'settings')
+    response['settings']['runmode'] = setas
+    close_connection(conn)                          #TODO unsure why I have to close and reopen this connection to get result
+    conn = create_connection()
+    conn.send('{"update":' + json.dumps(response) + '}')
+    close_connection(conn)
+
 
 def set_preview(setas=500, isOn=False):
-    conn = websocket.create_connection(url, timeout=6)
+    conn = create_connection()
     conn.send('{"request": "light_schedule" }')
-    answer = str(conn.recv())
-    answer = json.loads(answer)
-    for c in answer['channels']:
+    response = get_response(conn, 'channels')
+    for c in response['channels']:
         c['preview']['value'] = setas
         c['preview']['active'] = isOn
-    update = answer
-    conn.close(reason='done listening')
-    conn = websocket.create_connection(url, timeout=6)
-    #conn.send('{"update":' + json.dumps(answer) + '}')
-    conn.send('{"update":' + json.dumps(update) + '}')
-    conn.close(reason='done listening')
+    close_connection(conn)
+    conn = create_connection()
+    conn.send('{"update":' + json.dumps(response) + '}')
+    close_connection(conn)
 
 
 @app.route('/')
 def homepage():
     return "ayyyyyyy"
 
-    
-
 
 @ask.launch
 def start_skill():
-    welcome_message = "Connected. "  #this should be a question
+    welcome_message = "Connected. "  # this should be a question
     welcome_message = render_template('welcome')
     #welcome_message = get_info()
     return question(welcome_message)
+
 
 @ask.intent("InfoIntent")
 def share_info():
     print("Received Information Request")
     return statement(get_info())
+
 
 @ask.intent("RunmodeIntent", mapping={'runmode': 'Runmode'})
 def runmode_intent(runmode):
@@ -85,10 +104,12 @@ def runmode_intent(runmode):
     else:
         return statement("Aquarium runmode set to " + runmode)
 
+
 @ask.intent("NoWeatherIntent")
-def no_weather_intent():
+def no_runmode_intent():
     set_runmode(setas="normal")
-    return statement("Ending weather")
+    return statement("Ending runmode")
+
 
 @ask.intent('LightAdjustmentIntent', convert={'percent': int})
 def light_adjustment_intent(percent):
@@ -96,40 +117,13 @@ def light_adjustment_intent(percent):
     set_preview(setas=pwm, isOn=True)
     return statement('Setting lights to {}'.format(pwm))
 
+
 @ask.intent("AMAZON.StopIntent")
 def normal_runmode_intent():
     set_preview(isOn=False)
-    time.sleep(1)
+    #time.sleep(1)
     set_runmode(setas='normal')
     return statement("Returning to normal run schedule.")
-
-@ask.intent('GeorgeIntent')
-def george_michael():
-    speech = 'yeah you got it!'
-
-    #stream_url = 'https://archive.org/download/WakeMeUpBeforeYouGoGo_417/WakeMeUpBeforeYouGoGo.ogg'
-    stream_url = 'https://ia800203.us.archive.org/27/items/CarelessWhisper_435/CarelessWhisper.ogg'
-
-    return audio(speech).play(stream_url)
-
-
-@ask.intent('AMAZON.PauseIntent')
-def pause():
-    return audio('Paused the stream.').stop()
-
-
-@ask.intent('AMAZON.ResumeIntent')
-def resume():
-    return audio('Resuming.').resume()
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
